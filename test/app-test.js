f7e34a6d4f324a8afcabd9df6166abad8282c906
Anil Bisalehalli Prasannakumar <anil.kumar@ringcentral.com>
2017-04-29 00:18:21 -0700

    Adding test Cases

---
+++ test/app-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var expect = require('chai').expect;
+var app = require('../app.js');
+var SDK = require("../node_modules/ringcentral/src/SDK");
+
+describe('Glipped.InviteUsers', function() {
+
+// TO DO
+
+
+});
+
+describe('RingCentral', function() {
+
+    function test(suite, server, done) {
+
+        suite.timeout(10000); // Per SLA should be 3 seconds
+
+        var sdk = new SDK({server: server, appKey: '', appSecret: ''});
+
+        return sdk.platform().get('', null, {skipAuthCheck: true}).then(function(response) {
+
+            expect(response.json().uri).to.equal(server + '/restapi/v1.0');
+
+            sdk.cache().clean();
+
+        });
+
+    }
+
+    it('connects to sandbox', function() {
+        return test(this, SDK.server.sandbox);
+    });
+
+    it('connects to production', function() {
+        return test(this, SDK.server.production);
+    });
+
+});
